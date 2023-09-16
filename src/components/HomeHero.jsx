f54de39b477bc840342e58e884893af936ff1b81
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-16 22:30:58 +0100

    Change Hero logo image

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import heroImage from "../assets/acorn-logo-yellow.png";
+import heroImage from "../assets/acorn-logo.png";
 import tireImage from "../assets/image-4.jpg";
 
 const HomeHero = () => {
