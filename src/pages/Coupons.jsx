1d882f743e4b8e81bc0ab0eb08e6b4000176a019
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-17 03:07:55 +0100

    Add Search component to coupon page

--- src/pages/Coupons.jsx
+++ src/pages/Coupons.jsx
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import backgroundImage from "../assets/image-5.jpg";
+import { FormButton } from "../components/reusables/Styles";
+import { SearchComponent } from "../components/Advert";
+import { LocationModal } from "./ScheduleService";
+import serviceLocations from "../data/service-location-data";
 
 const Coupons = () => {
+  const [showLocationModal, setShowLocationModal] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState(
+    () => serviceLocations[0]
+  );
+  const portalRef = useRef(null);
+
+  const hideShowModal = (e) => {
+    e && e.preventDefault();
+    setShowLocationModal(false);
+    document.body.style.overflow = "initial";
+  };
+  const showShowModal = (e) => {
+    e && e.preventDefault();
+    setShowLocationModal(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const handleInputChange = (e) => {
+    setCurrentLocation(e.target.value);
+  };
   return (
-    <CouponsContainer>
-      <Heading>There are no Coupons to display at the moment</Heading>
-    </CouponsContainer>
+    <CouponsPageContainer>
+      <SearchComponent
+        showShowModal={showShowModal}
+        currentLocation={currentLocation}
+        linkType={"couponLink"}
+        dropdownText={"click to change location"}
+        linkText={"Checkout available coupons"}
+      />
+      {showLocationModal && (
+        <LocationModal
+          portalRef={portalRef}
+          hideShowModal={hideShowModal}
+          handleInputChange={handleInputChange}
+        />
+      )}
+      {/* <Heading>No tire to display at the moment</Heading> */}
+    </CouponsPageContainer>
   );
 };
 
-const CouponsContainer = styled.div`
+const CouponsPageContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 400px;
-`;
-
-const Heading = styled.h2`
-  font-family: var(--teko);
-  text-align: center;
+  background: url(${backgroundImage});
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: cover;
+  padding: 1rem;
+  margin: 1rem 0;
 `;
 
 export default Coupons;
