1b202a2f805feca47e6be659c625e03ce51eca55
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-17 03:51:43 +0100

    Fix link typo in boxnav component

--- src/components/BoxNav.jsx
+++ src/components/BoxNav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const ROUTES = [
   {
-    path: "/tire",
+    path: "/tires",
     name: "Shop for Tires",
     Icon: <i className="fi fi-rr-tire"></i>,
   },
