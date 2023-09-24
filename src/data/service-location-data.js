9a3057916b1adea2483ff9f21c14a9071de8dbce
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-24 22:15:13 +0100

    Add mapLink and iframeLink to location data

--- src/data/service-location-data.js
+++ src/data/service-location-data.js
@@ -9,6 +9,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6550",
     facebookLink: "https://www.facebook.com/profile.php?id=100089258956485",
+    mapLink: "https://maps.app.goo.gl/EXopiQU49uys2JFg8",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2940.0183325373982!2d-83.0297132!3d42.53366529999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824da5369ffe933%3A0x2af0442553c1e13a!2sMidas!5e0!3m2!1sen!2sng!4v1695589518194!5m2!1sen!2sng",
   },
   {
     shopLocation:
@@ -21,6 +24,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6627",
     facebookLink: "https://www.facebook.com/profile.php?id=100089785682170",
+    mapLink: "https://maps.app.goo.gl/poNXB5kLpWmxV7Yh9",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2941.398022934906!2d-83.1094258!3d42.504348900000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824c5636d06f4c3%3A0xd90033ee70548f8d!2sMidas!5e0!3m2!1sen!2sng!4v1695589493900!5m2!1sen!2sng",
   },
   {
     shopLocation:
@@ -33,6 +39,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6112",
     facebookLink: "https://www.facebook.com/profile.php?id=100089653862100",
+    mapLink: "https://maps.app.goo.gl/XvNng7HTXiTvdvcXA",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11734.132632496134!2d-83.1318837!3d42.6712474!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824e9c57aabdd69%3A0x327331c0ed2e4b7!2sMidas!5e0!3m2!1sen!2sng!4v1695589468228!5m2!1sen!2sng",
   },
   {
     shopLocation: "Midas of Novi,\n43421 W 12 Mile Road,\nNovi, MI 48377",
@@ -44,6 +53,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6952",
     facebookLink: "https://www.facebook.com/profile.php?id=100089672856242",
+    mapLink: "https://maps.app.goo.gl/giMV8222mxSSF5w38",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2941.831451457121!2d-83.4767086!3d42.4951358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824af104d9bc731%3A0x5615c1226b764bd6!2sMidas!5e0!3m2!1sen!2sng!4v1695589447621!5m2!1sen!2sng",
   },
   {
     shopLocation:
@@ -56,6 +68,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6861",
     facebookLink: "https://www.facebook.com/profile.php?id=100089322674584",
+    mapLink: "https://maps.app.goo.gl/Q3sCDRdVgKUNwGgs5",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d366.1017151495378!2d-83.2643395!3d42.7711447!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824ecda15611277%3A0x39db527c7a81e73f!2sMidas!5e0!3m2!1sen!2sng!4v1695589415158!5m2!1sen!2sng",
   },
   {
     shopLocation:
@@ -68,6 +83,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6516",
     facebookLink: "https://www.facebook.com/profile.php?id=100089127297569",
+    mapLink: "https://maps.app.goo.gl/QLp1srcL2MkXgTHLA",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2916.7866344333374!2d-85.6322742!3d43.024877999999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8819ab6fe9e3682d%3A0xf100ed9936d46431!2sMidas!5e0!3m2!1sen!2sng!4v1695589374914!5m2!1sen!2sng",
   },
   {
     shopLocation: "Midas of Kalamazoo,\n6009 Gull Road,\nKalamazoo, MI 49048",
@@ -79,6 +97,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6271",
     facebookLink: "https://www.facebook.com/profile.php?id=100089121621007",
+    mapLink: "https://maps.app.goo.gl/PDQ2CvtsSx6ahYJk9",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.654635644596!2d-85.5120475!3d42.3285645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88178499b2aff619%3A0xd3214c8f20ec7473!2sMidas!5e0!3m2!1sen!2sng!4v1695589350658!5m2!1sen!2sng",
   },
   {
     shopLocation: "Midas of Portage,\n5112 S Westnedge Ave,\nPortage, MI 49002",
@@ -90,6 +111,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6265",
     facebookLink: "https://www.facebook.com/profile.php?id=100089105241992",
+    mapLink: "https://maps.app.goo.gl/RGg9BzQHGpcz4h6y7",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d184.60262445572246!2d-85.5928676!3d42.2434609!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88179dee58c5a74f%3A0xdc50ef8f232461ff!2sMidas!5e0!3m2!1sen!2sng!4v1695589241580!5m2!1sen!2sng",
   },
   {
     shopLocation: "Midas of Jackson,\n1503 Clinton Ave,\nJackson, MI 49202",
@@ -101,6 +125,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6264",
     facebookLink: "https://www.facebook.com/profile.php?id=100089572383461",
+    mapLink: "https://maps.app.goo.gl/bFArAmvuV9hr8Fao7",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2952.5657132498022!2d-84.42264639999999!3d42.266445999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883d2ff187ec14cb%3A0x1006e335a3d70512!2sMidas!5e0!3m2!1sen!2sng!4v1695589205696!5m2!1sen!2sng",
   },
   {
     shopLocation:
@@ -113,6 +140,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6514",
     facebookLink: "https://www.facebook.com/profile.php?id=100089482303692",
+    mapLink: "https://maps.app.goo.gl/FxaemeE9y7wSBcWQ8",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2922.1363534984675!2d-85.60278502464055!3d42.91216520004389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88184d34f3189d3d%3A0xe14b21c8cf1c2d4a!2sMidas!5e0!3m2!1sen!2sng!4v1695588940397!5m2!1sen!2sng",
   },
   {
     shopLocation: "Midas Roseville,\n25525 Gratiot Ave,\nRoseville, MI 48066",
@@ -124,6 +154,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6142",
     facebookLink: "https://www.facebook.com/profile.php?id=100089191818027",
+    mapLink: "https://maps.app.goo.gl/ssVHcSipTN4iFNH39",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2942.3616122736685!2d-82.94874782466569!3d42.48386432727216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d783a400d9f3%3A0xe7c9311065659b3!2sMidas!5e0!3m2!1sen!2sng!4v1695589118707!5m2!1sen!2sng",
   },
   {
     shopLocation: "Midas Alpine,\n2360 Alpine Ave NW,\nGrand Rapids, MI 49544",
@@ -135,6 +168,9 @@ const serviceLocations = [
     financingLink:
       "https://www.midas.com/about-midas/midas-credit-card-service-financing?shopnum=6885",
     facebookLink: "",
+    mapLink: "https://maps.app.goo.gl/UM2AcbLGCGERoTu36",
+    iframe:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2917.7130194738156!2d-85.69061552463504!3d43.00537709408993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8819aea61c734f11%3A0x3fba768ef90c317b!2sMidas!5e0!3m2!1sen!2sng!4v1695589163939!5m2!1sen!2sng",
   },
 ];
 
