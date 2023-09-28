57360e9b1bde598e6d81e72fa60bc2c65daaaa37
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-28 23:23:32 +0100

    Change background of hero image

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -54,11 +54,12 @@ const HeroContainer = styled.div`
     bottom: 0;
     width: 100%;
     height: 100%;
-    background: linear-gradient(
-      to right,
-      rgba(255, 255, 255, 0.3),
-      rgba(0, 0, 0, 0.4)
-    );
+    // background: linear-gradient(
+    //   to right,
+    //   rgba(255, 255, 255, 0.3),
+    //   rgba(0, 0, 0, 0.4)
+    // );
+    background: rgba(255, 255, 255, 0.2);
     // background: linear-gradient(
     //   to right,
     //   rgb(51 51 51 / 32%),
