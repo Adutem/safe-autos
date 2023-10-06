84b02f1a13b17d66e8e37fa6b3ce90e6ee40c6eb
philipthedeveloper <philipowolabi79@gmail.com>
2023-10-06 10:37:14 +0100

    Refactor tire page
    Allows the global context to handle the display and selection of current store location
    Remove the StoreLocationModal from Tire page

--- src/pages/Tires.jsx
+++ src/pages/Tires.jsx
@@ -1,27 +1,11 @@
 import React, { useState, useRef } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import backgroundImage from "../assets/image-5.jpg";
-import { FormButton } from "../components/reusables/Styles";
 import { SearchComponent } from "../components/Advert";
-import { LocationModal } from "./ScheduleService";
-import serviceLocations from "../data/service-location-data";
+import { useGlobalContext } from "../contexts/GlobalContext";
 
 export default function Tires() {
-  const [showLocationModal, setShowLocationModal] = useState(false);
-  const [currentLocation, setCurrentLocation] = useState(null);
-  const portalRef = useRef(null);
-
-  const hideShowModal = (e) => {
-    e && e.preventDefault();
-    setShowLocationModal(false);
-    document.body.style.overflow = "initial";
-  };
-  const showShowModal = (e) => {
-    e && e.preventDefault();
-    setShowLocationModal(true);
-    document.body.style.overflow = "hidden";
-  };
+  const { currentStoreLocation, displayLocationModal } = useGlobalContext();
 
   const handleInputChange = (e) => {
     setCurrentLocation(e.target.value);
@@ -29,18 +13,10 @@ export default function Tires() {
   return (
     <TiresPageContainer>
       <SearchComponent
-        showShowModal={showShowModal}
-        currentLocation={currentLocation}
+        showShowModal={displayLocationModal}
+        currentLocation={currentStoreLocation}
         linkType={"link"}
       />
-      {showLocationModal && (
-        <LocationModal
-          portalRef={portalRef}
-          hideShowModal={hideShowModal}
-          handleInputChange={handleInputChange}
-        />
-      )}
-      {/* <Heading>No tire to display at the moment</Heading> */}
     </TiresPageContainer>
   );
 }
