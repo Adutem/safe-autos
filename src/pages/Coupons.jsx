ed1a980a5db4b5876fe2fb864f01b9ae78614ce8
philipthedeveloper <philipowolabi79@gmail.com>
2023-10-13 10:42:35 +0100

    Upload coupon page to use location from global context

--- src/pages/Coupons.jsx
+++ src/pages/Coupons.jsx
@@ -1,47 +1,25 @@
 import React, { useState, useRef } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import backgroundImage from "../assets/image-5.jpg";
-import { FormButton } from "../components/reusables/Styles";
+// import { FormButton } from "../components/reusables/Styles";
 import { SearchComponent } from "../components/Advert";
 import { LocationModal } from "./ScheduleService";
-import serviceLocations from "../data/service-location-data";
+// import serviceLocations from "../data/service-location-data";
+import { useGlobalContext } from "../contexts/GlobalContext";
 
 const Coupons = () => {
-  const [showLocationModal, setShowLocationModal] = useState(false);
-  const [currentLocation, setCurrentLocation] = useState(null);
-  const portalRef = useRef(null);
+  const { currentStoreLocation, displayLocationModal } = useGlobalContext();
 
-  const hideShowModal = (e) => {
-    e && e.preventDefault();
-    setShowLocationModal(false);
-    document.body.style.overflow = "initial";
-  };
-  const showShowModal = (e) => {
-    e && e.preventDefault();
-    setShowLocationModal(true);
-    document.body.style.overflow = "hidden";
-  };
-
-  const handleInputChange = (e) => {
-    setCurrentLocation(e.target.value);
-  };
   return (
     <CouponsPageContainer>
       <SearchComponent
-        showShowModal={showShowModal}
-        currentLocation={currentLocation}
+        showShowModal={displayLocationModal}
+        currentLocation={currentStoreLocation}
         linkType={"couponLink"}
         dropdownText={"select store location"}
         linkText={"Checkout available coupons"}
       />
-      {showLocationModal && (
-        <LocationModal
-          portalRef={portalRef}
-          hideShowModal={hideShowModal}
-          handleInputChange={handleInputChange}
-        />
-      )}
       {/* <Heading>No tire to display at the moment</Heading> */}
     </CouponsPageContainer>
   );
