70a2e27320dfc2e93f62e68c195b7b5929d5d614
philipthedeveloper <philipowolabi79@gmail.com>
2023-10-16 08:02:41 +0100

    Update financing page to use current store from global context

--- src/pages/Financing.jsx
+++ src/pages/Financing.jsx
@@ -4,42 +4,20 @@ import backgroundImage from "../assets/image-5.jpg";
 import { SearchComponent } from "../components/Advert";
 import { LocationModal } from "./ScheduleService";
 import serviceLocations from "../data/service-location-data";
+import { useGlobalContext } from "../contexts/GlobalContext";
 
 const Financing = () => {
-  const [showLocationModal, setShowLocationModal] = useState(false);
-  const [currentLocation, setCurrentLocation] = useState(null);
-  const portalRef = useRef(null);
+  const { currentStoreLocation, displayLocationModal } = useGlobalContext();
 
-  const hideShowModal = (e) => {
-    e && e.preventDefault();
-    setShowLocationModal(false);
-    document.body.style.overflow = "initial";
-  };
-  const showShowModal = (e) => {
-    e && e.preventDefault();
-    setShowLocationModal(true);
-    document.body.style.overflow = "hidden";
-  };
-
-  const handleInputChange = (e) => {
-    setCurrentLocation(e.target.value);
-  };
   return (
     <FinancingPageContainer>
       <SearchComponent
-        showShowModal={showShowModal}
-        currentLocation={currentLocation}
+        showShowModal={displayLocationModal}
+        currentLocation={currentStoreLocation}
         linkType={"financingLink"}
         dropdownText={"select store location"}
         linkText={"Checkout available Financing"}
       />
-      {showLocationModal && (
-        <LocationModal
-          portalRef={portalRef}
-          hideShowModal={hideShowModal}
-          handleInputChange={handleInputChange}
-        />
-      )}
       {/* <Heading>No tire to display at the moment</Heading> */}
     </FinancingPageContainer>
   );
